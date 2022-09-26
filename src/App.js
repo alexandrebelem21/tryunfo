a4fa578ea88226f412b92e99f7ea7dbe1a8b1f9c
alexandrebelem21 <alexandrebelem21@gmail.com>
2022-09-26 14:57:13 -0300

    Iniciando Projeto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Tryunfo</h1>
+        <h1>Tryunfo </h1>
       </div>
     );
   }
